0b569c234d4a0a2ff3626aca50c205a2d523da5a
Marcel <marcel.schneuer@uni-jena.de>
2019-11-11 23:43:26 +0100

    further aws implementation

--- android/app/src/main/java/com/swat/steptracker/MainActivity.java
+++ android/app/src/main/java/com/swat/steptracker/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AWSAppSyncClient mAWSAppSyncClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
     }
+    
+    };
 }
